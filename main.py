ecb3159edcba16731554bc054b8d877e106d3f80
NairaJanaina <46908289+NairaJanaina@users.noreply.github.com>
2021-08-29 17:08:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import func_registro
 import entrevista
-
+import consultas
 # main
 while True:
     print("\nQue operação deseja realizar?")
     # adicionar as novas operações (entrevista, adotar) aqui
     print("1: Adicionar um novo animal\n2: Modificar um animal no registro\n"
         + "3: Realizar uma entrevista pré-adoção\n4: Adotar um animal\n5: Listar os animais adotados")
-    op = input("(1, 2, 3, 4, 5, sair): ").lower().strip()
+    op = input("(1, 2, 3, 4, 5, 6, sair): ").lower().strip()
     if op == "1":
         func_registro.regAdicionar()
         print("Operação Concluída.")
@@ -22,6 +22,9 @@ while True:
         print("Operação Concluída.")
     elif op == "5":
         func_registro.regListarAdotados()
+        print("Operação Concluída.")
+    elif op == "6":
+        consultas.regChecarNaoAdotados()
         print("Operação Concluída.")
     elif op == "sair":
         exit()
