540558db797c63ddb0d8a2d9388a6199ff10212d
NatePlays95 <natanmaiaplays@outlook.com>
2021-08-29 14:46:14 -0300

    todos os processos

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import func_registro
+import entrevista
 
 # main
 while True:
-    print("Que operação deseja realizar?")
+    print("\nQue operação deseja realizar?")
     # adicionar as novas operações (entrevista, adotar) aqui
-    print("1: Adicionar um novo animal\n2: Modificar um animal no registro\n3: Listar os animais adotados")
-    op = input("(1, 2, 3, sair): ")
+    print("1: Adicionar um novo animal\n2: Modificar um animal no registro\n"
+        + "3: Realizar uma entrevista pré-adoção\n4: Adotar um animal\n5: Listar os animais adotados")
+    op = input("(1, 2, 3, 4, 5, sair): ").lower().strip()
     if op == "1":
         func_registro.regAdicionar()
+        print("Operação Concluída.")
     elif op == "2":
         func_registro.regModificar()
+        print("Operação Concluída.")
     elif op == "3":
-        func_registro.regChecarAdotados()
+        entrevista.cadastroPessoa()
+        print("Operação Concluída.")
+    elif op == "4":
+        entrevista.adotarAnimal()
+        print("Operação Concluída.")
+    elif op == "5":
+        func_registro.regListarAdotados()
+        print("Operação Concluída.")
     elif op == "sair":
         exit()
